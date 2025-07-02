ed8f4cc807da4663df69334e5116b085df93fcd8
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-07-02 21:23:20 +0800

    add celebration

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ checkInButton.addEventListener('click', () => {
     streak++;
     localStorage.setItem('streak', streak);
     updateStreakDisplay();
+    confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { y: 0.6 }
+    });
 });
 
 resetButton.addEventListener('click', () => {
