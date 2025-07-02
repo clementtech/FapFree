66391a6619927491d2ba7d34d6c891e5003f23f9
Clement Chong <119944151+clementtech@users.noreply.github.com>
2025-07-02 21:46:34 +0800

    added conditions, only 1 check in/day

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const cheerSound = new Audio('cheering.mp3');
 const disappointedSound = new Audio('haiya.mp3');
 
 let streak = localStorage.getItem('streak') || 0;
+let lastCheckInDate = localStorage.getItem('lastCheckInDate');
+
+function canCheckIn() {
+    if (!lastCheckInDate) {
+        return true;
+    }
+    const today = new Date().toDateString();
+    return today !== lastCheckInDate;
+}
+
+function updateCheckInButtonState() {
+    if (!canCheckIn()) {
+        checkInButton.disabled = true;
+    }
+}
 
 function playCheerSound() {
     cheerSound.play();
@@ -19,15 +34,20 @@ function updateStreakDisplay() {
 }
 
 checkInButton.addEventListener('click', () => {
-    streak++;
-    localStorage.setItem('streak', streak);
-    updateStreakDisplay();
-    playCheerSound();
-    confetti({
-        particleCount: 100,
-        spread: 70,
-        origin: { y: 0.6 }
-    });
+    if (canCheckIn()) {
+        streak++;
+        localStorage.setItem('streak', streak);
+        lastCheckInDate = new Date().toDateString();
+        localStorage.setItem('lastCheckInDate', lastCheckInDate);
+        updateStreakDisplay();
+        playCheerSound();
+        confetti({
+            particleCount: 100,
+            spread: 70,
+            origin: { y: 0.6 }
+        });
+        checkInButton.disabled = true;
+    }
 });
 
 resetButton.addEventListener('click', () => {
@@ -39,3 +59,4 @@ resetButton.addEventListener('click', () => {
 
 // Initial streak update on page load
 updateStreakDisplay();
+updateCheckInButtonState();
